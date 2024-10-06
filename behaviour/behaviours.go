4d39724c5f320c793e5a57942cafad8db4bfba13
Neil Smith <neil@nsmith.co.uk>
2024-10-06 15:54:01 +0100

    Added VP 14

--- behaviour/behaviours.go
+++ behaviour/behaviours.go
@@ -2,11 +2,13 @@ package behaviour
 
 import (
 	"github.com/nsmithuk/naughty-nameserver/behaviour/invalid_negative"
+	"github.com/nsmithuk/naughty-nameserver/behaviour/valid_positive"
 	"github.com/nsmithuk/naughty-nameserver/naughty"
 )
 
 func GeBehaviours() []naughty.BehaviourFactory {
 	return []naughty.BehaviourFactory{
+		new(valid_positive.OptoutDSNsec3RecordDeligation),
 		//new(valid_positive.AlgorithmCombinations),
 		//new(invalid_positive.MissingNSECRecordForWildcard),
 		//new(valid_negative.SingleNXDomainNsecResponse),
