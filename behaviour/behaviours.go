ee463a00ac2cdbe193841cc19da4dbf58ec1de4a
Neil Smith <neil@nsmith.co.uk>
2024-10-06 17:27:05 +0100

    Added IP 2 & 3

--- behaviour/behaviours.go
+++ behaviour/behaviours.go
@@ -1,21 +1,27 @@
 package behaviour
 
 import (
-	"github.com/nsmithuk/naughty-nameserver/behaviour/valid_positive"
+	"github.com/nsmithuk/naughty-nameserver/behaviour/invalid_positive"
 	"github.com/nsmithuk/naughty-nameserver/naughty"
 )
 
 func GeBehaviours() []naughty.BehaviourFactory {
 	return []naughty.BehaviourFactory{
+		// Valid Positive
 		//new(valid_positive.AlgorithmCombinations),
 		//new(valid_positive.NSECRecordForWildcard),
 		//new(valid_positive.NSEC3RecordForWildcard),
 		//new(valid_positive.KeyFlag256),
 		//new(valid_positive.KeyFlag257),
 		//new(valid_positive.MultipleClashingKeys),
-		new(valid_positive.MultipleDsRecords),
+		//new(valid_positive.MultipleDsRecords),
+		//new(valid_positive.OptoutDSNsec3RecordDeligation),
 
+		// Invalid Positive
 		//new(invalid_positive.MissingNSECRecordForWildcard),
+		//new(invalid_positive.InvalidKeyFlag),
+		new(invalid_positive.DsKeyMissmatch),
+
 		//new(valid_negative.SingleNXDomainNsecResponse),
 		//new(valid_negative.DoubleNXDomainNsecResponse),
 		//new(valid_negative.NODataNsecResponse),
---
+++ behaviour/invalid_positive/03_ds_key_missmatch.go
@@ -0,0 +1,41 @@
+package invalid_positive
+
+import (
+	"fmt"
+	"github.com/miekg/dns"
+	"github.com/nsmithuk/naughty-nameserver/behaviour/logging"
+	"github.com/nsmithuk/naughty-nameserver/naughty"
+	"net"
+)
+
+// IP 3
+
+type DsKeyMissmatch struct{}
+
+func (r *DsKeyMissmatch) Setup(ns *naughty.Nameserver) []*naughty.Zone {
+	name := dns.Fqdn(fmt.Sprintf("ds-key-missmatch.%s", ns.BaseZoneName))
+
+	signer, err := naughty.NewSignerAutogenPair(name, dns.ECDSAP256SHA256, 256, dns.ECDSAP256SHA256, 256)
+	if err != nil {
+		panic(err)
+	}
+
+	callbacks := naughty.NewStandardCallbacks(signer)
+
+	callbacks.DelegatedSingers = func() []*dns.DS {
+		// We only return the DS records for the ZSK.
+		return signer.Zsk.DelegatedSingers()
+	}
+
+	zone := naughty.NewZone(name, ns.NSRecords, callbacks)
+
+	a := &dns.A{
+		Hdr: naughty.NewHeader(fmt.Sprintf("test.%s", name), dns.TypeA),
+		A:   net.ParseIP("192.0.2.53").To4(),
+	}
+	zone.AddRecord(a)
+
+	naughty.Info(fmt.Sprintf(logging.LogFmtValid, a.Header().Name))
+
+	return []*naughty.Zone{zone}
+}
